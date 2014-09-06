f67840e167c6ef9ff0ede045d7d96728a295dadd
Thomas Deegan <tadeegan@gmail.com>
2014-09-05 23:44:47 -0500

    Initial Commit

---
+++ Android Stream/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  Android Stream
+//
+//  Created by Thomas on 9/5/14.
+//  Copyright (c) 2014 hackers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Android Stream/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Android Stream
+//
+//  Created by Thomas on 9/5/14.
+//  Copyright (c) 2014 hackers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
